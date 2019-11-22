68522663a9bc4cd8101accd84c31bc3e5ce56690
Keely Brown <bkeely1992@gmail.com>
2019-11-22 11:57:15 -0500

    Set up the structure for the InputManager. Going to set up the window in the RenderSystem before continuing.

--- Engine/InputManager.cpp
+++ Engine/InputManager.cpp
@@ -1,10 +1,51 @@
 #include "Core.h"
 #include "InputManager.h"
+#include "RenderSystem.h"
 
 void InputManager::initialize()
 {
+	//initialize all of the key states?
 }
 
 void InputManager::update(float deltaTime)
 {
+	sf::Event event;
+	//while (RenderSystem::instance().getRenderWindow()->pollEvent(event))
+	//{
+	//	switch (event.type)
+	//	{
+	//	case sf::Event::Closed:
+	//		RenderSystem::instance().closeWindow();
+	//
+	//		return;
+
+	//		break;
+
+	//	case sf::Event::KeyReleased:
+	//		
+	//		break;
+
+	//	case sf::Event::MouseButtonPressed:
+	//
+	//		break;
+
+	//	default:
+	//		break;
+	//	}
+	//}
+}
+
+InputManager::InputState InputManager::getKeyState(sf::Keyboard::Key inKey) 
+{
+	return keyMap[inKey];
+}
+
+InputManager::InputState InputManager::getMouseButtonState(sf::Mouse::Button inMouseBtn) 
+{
+	return mouseBtnMap[inMouseBtn];
+}
+
+sf::Vector2<float> InputManager::getMousePosition()
+{
+	return mousePosition;
 }
--- Engine/InputManager.h
+++ Engine/InputManager.h
@@ -14,12 +14,30 @@
 
 class InputManager final : public ISystem
 {
+public:
+	enum InputState
+	{
+		None, Down, Held, Up
+	};
+
+private:
+	std::map<sf::Keyboard::Key, InputState> keyMap;
+	std::map<sf::Mouse::Button, InputState> mouseBtnMap;
+	sf::Vector2<float> mousePosition;
+
+public:
+	InputState getKeyState(sf::Keyboard::Key inKey);
+	InputState getMouseButtonState(sf::Mouse::Button inButton);
+	sf::Vector2<float> getMousePosition();
+
 protected:
     void initialize() override;
     void update(float deltaTime) override;
 
     friend class GameEngine;
     DECLARE_SINGLETON(InputManager)
+
+
 };
 
 #endif
