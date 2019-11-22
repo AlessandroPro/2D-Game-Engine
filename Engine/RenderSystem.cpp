791408efaa1d2f91107da1b3ad6240383e954367
Keely Brown <bkeely1992@gmail.com>
2019-11-22 13:59:43 -0500

    Edit: Implemented basic definitions for initialize, update, closeWindow, addRenderable, removeRenderable and getRenderWindow. Primarily looking to get basic window functionality such that the InputManager can be implemented.

--- Engine/RenderSystem.cpp
+++ Engine/RenderSystem.cpp
@@ -1,10 +1,42 @@
 #include "Core.h"
 #include "RenderSystem.h"
+#include "IRenderable.h"
+
 
 void RenderSystem::initialize()
 {
+	window = new sf::RenderWindow(sf::VideoMode(width, height), name);
+	window->setFramerateLimit(60);
 }
 
 void RenderSystem::update(float deltaTime)
 {
+	if (window != nullptr)
+	{
+		window->clear();
+
+		for (auto renderable : renderables)
+		{
+			renderable->render(window);
+		}
+
+		window->display();
+	}
+}
+
+void RenderSystem::closeWindow()
+{
+	window->close();
+	delete window;
+	window = nullptr;
+}
+
+void RenderSystem::addRenderable(IRenderable* _renderable)
+{
+	renderables.push_back(_renderable);
+}
+
+void RenderSystem::removeRenderable(IRenderable* _renderable)
+{
+	renderables.remove(_renderable);
 }
