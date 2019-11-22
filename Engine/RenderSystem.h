4d45003745b78274a471e0c42259316ca887eb5d
Keely Brown <bkeely1992@gmail.com>
2019-11-22 13:59:43 -0500

    Implemented basic definitions for initialize, update, closeWindow, addRenderable, removeRenderable and getRenderWindow. Primarily looking to get basic window functionality such that the InputManager can be implemented.

--- Engine/RenderSystem.h
+++ Engine/RenderSystem.h
@@ -16,12 +16,32 @@ class IRenderable;
 
 class RenderSystem final : public ISystem
 {
+private:
+	std::string name = "";
+	int width = 300;
+	int height = 300;
+	bool fullscreen = false;
+
+	sf::RenderWindow* window = nullptr;
+
+	std::list<IRenderable*> renderables;
+
 protected:
 	void initialize() override;
 	void update(float deltaTime) override;
 
     friend class GameEngine;
+	friend class InputManager;
     DECLARE_SINGLETON(RenderSystem)
+
+public:
+	void closeWindow();
+	sf::RenderWindow* getRenderWindow() { return window; }
+	void addRenderable(IRenderable* _renderable);
+	void removeRenderable(IRenderable* _renderable);
+
+	int getHeight() { return height; }
+	int getWidth() { return width; }
 };
 
 #endif
