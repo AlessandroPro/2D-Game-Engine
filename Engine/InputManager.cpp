946174958afa45bf8b7b19aedcd507e1e9118482
Keely Brown <bkeely1992@gmail.com>
2019-11-22 23:43:02 -0500

    Edit: Completed general functionality of the InputManager. Key press, mouse button press and mouse movement is all recorded and accessible.

--- Engine/InputManager.cpp
+++ Engine/InputManager.cpp
@@ -9,39 +9,81 @@ void InputManager::initialize()
 
 void InputManager::update(float deltaTime)
 {
-	sf::Event event;
-	//while (RenderSystem::instance().getRenderWindow()->pollEvent(event))
-	//{
-	//	switch (event.type)
-	//	{
-	//	case sf::Event::Closed:
-	//		RenderSystem::instance().closeWindow();
-	//
-	//		return;
+	for(auto key : keyMap)
+	{
+		if(key.second == InputManager::Up)
+		{
+			keyMap[key.first] = InputManager::None;
+		}
+		else if (key.second == InputManager::Down)
+		{
+			keyMap[key.first] = InputManager::Held;
+		}
+	}
 
-	//		break;
+	for (auto btn : mouseBtnMap)
+	{
+		if (btn.second == InputManager::Up)
+		{
+			mouseBtnMap[btn.first] = InputManager::None;
+		}
+		else if (btn.second == InputManager::Down)
+		{
+			mouseBtnMap[btn.first] = InputManager::Held;
+		}
+	}
 
-	//	case sf::Event::KeyReleased:
-	//		
-	//		break;
-
-	//	case sf::Event::MouseButtonPressed:
-	//
-	//		break;
-
-	//	default:
-	//		break;
-	//	}
-	//}
+	sf::Event ev;
+	while (RenderSystem::instance().getRenderWindow()->pollEvent(ev))
+	{
+		switch (ev.type)
+		{
+		case sf::Event::Closed:
+			RenderSystem::instance().closeWindow();
+			return;
+		case sf::Event::KeyPressed:
+			if(keyMap[ev.key.code] != InputState::Held)
+			{
+				keyMap[ev.key.code] = InputState::Down;
+			}
+			break;
+		case sf::Event::KeyReleased:
+			keyMap[ev.key.code] = InputState::Up;
+			break;
+		case sf::Event::MouseButtonPressed:
+			if (mouseBtnMap[ev.mouseButton.button] != InputState::Held)
+			{
+				mouseBtnMap[ev.mouseButton.button] = InputState::Down;
+			}
+			break;
+		case sf::Event::MouseButtonReleased:
+			mouseBtnMap[ev.mouseButton.button] = InputState::Up;
+			break;
+		case sf::Event::MouseMoved:
+			mousePosition.x = ev.mouseMove.x;
+			mousePosition.y = ev.mouseMove.y;
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 InputManager::InputState InputManager::getKeyState(sf::Keyboard::Key inKey) 
 {
+	if (keyMap.find(inKey) == keyMap.end())
+	{
+		keyMap.insert(std::pair<sf::Keyboard::Key, InputState>(inKey, InputState::None));
+	}
 	return keyMap[inKey];
 }
 
 InputManager::InputState InputManager::getMouseButtonState(sf::Mouse::Button inMouseBtn) 
 {
+	if (mouseBtnMap.find(inMouseBtn) == mouseBtnMap.end())
+	{
+		mouseBtnMap.insert(std::pair<sf::Mouse::Button, InputState>(inMouseBtn, InputState::None));
+	}
 	return mouseBtnMap[inMouseBtn];
 }
 
