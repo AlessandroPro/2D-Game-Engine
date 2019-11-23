81b23b9d7dbe8b223e85a2b7487b1a47cdb1d6bb
Keely Brown <bkeely1992@gmail.com>
2019-11-23 12:17:56 -0500

    Edit: Created updateKeyStates, updateMouseButtonStates methods used for updating the state of input after the initial push/release states are recorded and need to be changed to None/Held on subsequent iterations of the game loop.
    Added handling for control, alt & shift modifiers, and changed the KeyMap to use a new KeyData struct which contains all of that information for every key.
    
    Changed InputState to PushState as it made more sense as the name.
    
    Added uniformMousePosition which records what percentage across the screen the cursor currently is, allowing for generalized handling of position on the size of the window changing.
    
    Minor changes - updated Vector2<float> to Vector2f to be consistent with GameObjectManager. Changed struct returns to be pass by reference.

--- Engine/InputManager.cpp
+++ Engine/InputManager.cpp
@@ -4,37 +4,19 @@
 
 void InputManager::initialize()
 {
-	//initialize all of the key states?
+
 }
 
 void InputManager::update(float deltaTime)
 {
-	for(auto key : keyMap)
-	{
-		if(key.second == InputManager::Up)
-		{
-			keyMap[key.first] = InputManager::None;
-		}
-		else if (key.second == InputManager::Down)
-		{
-			keyMap[key.first] = InputManager::Held;
-		}
-	}
+	//These methods update from Up->None and Down->Held
+	updateKeyStates();
+	updateMouseButtonStates();
 
-	for (auto btn : mouseBtnMap)
-	{
-		if (btn.second == InputManager::Up)
-		{
-			mouseBtnMap[btn.first] = InputManager::None;
-		}
-		else if (btn.second == InputManager::Down)
-		{
-			mouseBtnMap[btn.first] = InputManager::Held;
-		}
-	}
-
+	sf::RenderWindow* window = RenderSystem::instance().getRenderWindow();
 	sf::Event ev;
-	while (RenderSystem::instance().getRenderWindow()->pollEvent(ev))
+
+	while (window->pollEvent(ev))
 	{
 		switch (ev.type)
 		{
@@ -42,26 +24,44 @@ void InputManager::update(float deltaTime)
 			RenderSystem::instance().closeWindow();
 			return;
 		case sf::Event::KeyPressed:
-			if(keyMap[ev.key.code] != InputState::Held)
+			//If a modifier key is pressed then the modifier map is updated
+			if(modifierKeyMap.find(ev.key.code) != modifierKeyMap.end())
 			{
-				keyMap[ev.key.code] = InputState::Down;
+				updateModifier(ev.key.code, true);
+			}
+			//Do not update the state to down if it's already been set as being held.
+			if(keyMap[ev.key.code].state != PushState::Held)
+			{
+				keyMap[ev.key.code].state = PushState::Down;
 			}
 			break;
 		case sf::Event::KeyReleased:
-			keyMap[ev.key.code] = InputState::Up;
+			//If a modifier key is released then the modifier map is updated
+			if (modifierKeyMap.find(ev.key.code) != modifierKeyMap.end())
+			{
+				updateModifier(ev.key.code, false);
+			}
+
+			keyMap[ev.key.code].state = PushState::Up;
 			break;
 		case sf::Event::MouseButtonPressed:
-			if (mouseBtnMap[ev.mouseButton.button] != InputState::Held)
+			//Do not update the state to down if it's already been set as being held.
+			if (mouseBtnMap[ev.mouseButton.button] != PushState::Held)
 			{
-				mouseBtnMap[ev.mouseButton.button] = InputState::Down;
+				mouseBtnMap[ev.mouseButton.button] = PushState::Down;
 			}
 			break;
 		case sf::Event::MouseButtonReleased:
-			mouseBtnMap[ev.mouseButton.button] = InputState::Up;
+			mouseBtnMap[ev.mouseButton.button] = PushState::Up;
 			break;
 		case sf::Event::MouseMoved:
-			mousePosition.x = ev.mouseMove.x;
 			mousePosition.y = ev.mouseMove.y;
+			mousePosition.x = ev.mouseMove.x;
+
+			//Unflip the Y coordinates for uniform mouse position.
+			uniformMousePosition.y = 1.00f - (mousePosition.y / window->getSize().y);
+			uniformMousePosition.x = mousePosition.x / window->getSize().x;
+			
 			break;
 		default:
 			break;
@@ -69,25 +69,154 @@ void InputManager::update(float deltaTime)
 	}
 }
 
-InputManager::InputState InputManager::getKeyState(sf::Keyboard::Key inKey) 
+InputManager::PushState InputManager::getKeyState(sf::Keyboard::Key inKey)
 {
+	//Initializes the key data if it doesn't already exist.
 	if (keyMap.find(inKey) == keyMap.end())
 	{
-		keyMap.insert(std::pair<sf::Keyboard::Key, InputState>(inKey, InputState::None));
+		KeyData newKey = { PushState::None, false, false, false };
+		keyMap.insert(std::pair<sf::Keyboard::Key, KeyData>(inKey, newKey));
+	}
+	return keyMap[inKey].state;
+}
+InputManager::KeyData& InputManager::getKeyData(sf::Keyboard::Key inKey)
+{
+	//Initializes the key data if it doesn't already exist.
+	if (keyMap.find(inKey) == keyMap.end())
+	{
+		KeyData newKey = { PushState::None, false, false, false };
+		keyMap.insert(std::pair<sf::Keyboard::Key, KeyData>(inKey, newKey));
 	}
 	return keyMap[inKey];
 }
-
-InputManager::InputState InputManager::getMouseButtonState(sf::Mouse::Button inMouseBtn) 
+InputManager::PushState InputManager::getMouseButtonState(sf::Mouse::Button inMouseBtn)
 {
+	//Initializes the button state if it doesn't already exist.
 	if (mouseBtnMap.find(inMouseBtn) == mouseBtnMap.end())
 	{
-		mouseBtnMap.insert(std::pair<sf::Mouse::Button, InputState>(inMouseBtn, InputState::None));
+		mouseBtnMap.insert(std::pair<sf::Mouse::Button, PushState>(inMouseBtn, PushState::None));
 	}
 	return mouseBtnMap[inMouseBtn];
 }
 
-sf::Vector2<float> InputManager::getMousePosition()
+sf::Vector2f& InputManager::getMousePosition()
 {
 	return mousePosition;
+}
+sf::Vector2f& InputManager::getUniformMousePosition()
+{
+	return uniformMousePosition;
+}
+
+//Iterates through all of the currently stored keys and updates their states.
+void InputManager::updateKeyStates()
+{
+	for (auto key : keyMap)
+	{
+		//The initial key release is stored for only one iteration of the inputmanager's loop
+		//it then shifts to the None state.
+		if (key.second.state == PushState::Up)
+		{
+			keyMap[key.first].state = PushState::None;
+		}
+		//The initial key push is stored for only one iteration of the inputmanager's loop
+		//it then shifts to the Held state.
+		else if (key.second.state == PushState::Down)
+		{
+			keyMap[key.first].state = PushState::Held;
+		}
+	}
+}
+//Iterates through all of the currently stored buttons and updates their states.
+void InputManager::updateMouseButtonStates()
+{
+	for (auto btn : mouseBtnMap)
+	{
+		//The initial button release is stored for only one iteration of the inputmanager's loop
+		//it then shifts to the None state.
+		if (btn.second == PushState::Up)
+		{
+			mouseBtnMap[btn.first] = PushState::None;
+		}
+		//The initial button push is stored for only one iteration of the inputmanager's loop
+		//it then shifts to the Held state.
+		else if (btn.second == PushState::Down)
+		{
+			mouseBtnMap[btn.first] = PushState::Held;
+		}
+	}
+}
+
+//Updates the state for all keys if a new modifier state is achieved as a result
+//of the provided input modifier key's new value.
+void InputManager::updateModifier(sf::Keyboard::Key modifierKey, bool isPressed)
+{
+	modifierKeyMap[modifierKey] = isPressed;
+
+	if(modifierKey == sf::Keyboard::LShift ||
+		modifierKey == sf::Keyboard::RShift)
+	{ 
+		//We only care about updating the the shift state for all keys if one of the shifts is true
+		//and the update to the state was one of them being pushed.
+		if((modifierKeyMap[sf::Keyboard::LShift] || modifierKeyMap[sf::Keyboard::RShift])
+			&& isPressed)
+		{
+			for(auto key : keyMap)
+			{
+				keyMap[key.first].shift = true;
+			}
+		}
+		//We only care about updating the the shift state for all keys if both of the shifts are false
+		else if (!modifierKeyMap[sf::Keyboard::LShift] && !modifierKeyMap[sf::Keyboard::RShift])
+		{
+			for (auto key : keyMap)
+			{
+				keyMap[key.first].shift = false;
+			}
+		}
+	}
+	else if(modifierKey == sf::Keyboard::LControl ||
+		modifierKey == sf::Keyboard::RControl)
+	{
+		//We only care about updating the the control state for all keys if one of the controls is true
+		//and the update to the state was one of them being pushed.
+		if ((modifierKeyMap[sf::Keyboard::LControl] || modifierKeyMap[sf::Keyboard::RControl])
+			&& isPressed)
+		{
+			for (auto key : keyMap)
+			{
+				keyMap[key.first].control = true;
+			}
+		}
+		//We only care about updating the the control state for all keys if both of the controls are false
+		else if (!modifierKeyMap[sf::Keyboard::LControl] && !modifierKeyMap[sf::Keyboard::RControl])
+		{
+			for (auto key : keyMap)
+			{
+				keyMap[key.first].control = false;
+			}
+		}
+	}
+	else if(modifierKey == sf::Keyboard::LAlt ||
+		modifierKey == sf::Keyboard::RAlt)
+	{
+		//We only care about updating the the alt state for all keys if one of the alts is true
+		//and the update to the state was one of them being pushed.
+		if ((modifierKeyMap[sf::Keyboard::LAlt] || modifierKeyMap[sf::Keyboard::RAlt])
+			&& isPressed)
+		{
+			for (auto key : keyMap)
+			{
+				keyMap[key.first].alt = true;
+			}
+		}
+		//We only care about updating the the alt state for all keys if both of the alts are false
+		else if (!modifierKeyMap[sf::Keyboard::LAlt] && !modifierKeyMap[sf::Keyboard::RAlt])
+		{
+			for (auto key : keyMap)
+			{
+				keyMap[key.first].alt = false;
+			}
+		}
+	}
 }
--- Engine/InputManager.h
+++ Engine/InputManager.h
@@ -15,20 +15,54 @@
 class InputManager final : public ISystem
 {
 public:
-	enum InputState
+	//Used to store the state of the input
+	enum PushState
 	{
 		None, Down, Held, Up
 	};
 
+	//Used to store additional information on keys.
+	struct KeyData
+	{
+		PushState state;
+		bool alt;
+		bool shift;
+		bool control;
+	};
+
 private:
-	std::map<sf::Keyboard::Key, InputState> keyMap;
-	std::map<sf::Mouse::Button, InputState> mouseBtnMap;
-	sf::Vector2<float> mousePosition;
+	std::map<sf::Keyboard::Key, KeyData> keyMap;
+
+	//A map of all of the modifier keys that are used to shorthand check whether a key is being pressed along with one of these modifiers.
+	std::map<sf::Keyboard::Key,bool> modifierKeyMap 
+	{ 
+		std::pair<sf::Keyboard::Key,bool>(sf::Keyboard::LControl, false), 
+		std::pair<sf::Keyboard::Key,bool>(sf::Keyboard::RControl, false),
+		std::pair<sf::Keyboard::Key,bool>(sf::Keyboard::LShift, false),
+		std::pair<sf::Keyboard::Key,bool>(sf::Keyboard::RShift, false),
+		std::pair<sf::Keyboard::Key,bool>(sf::Keyboard::LAlt, false),
+		std::pair<sf::Keyboard::Key,bool>(sf::Keyboard::RAlt, false)
+	};
+
+	std::map<sf::Mouse::Button, PushState> mouseBtnMap;
+
+	//The mouse position is a pixel-by-pixel measurement of where the cursor currently is on the screen
+	sf::Vector2f mousePosition;
+
+	//The uniform mouse position is a measurement of where the cursor is on the screen in terms of a percentage of how far it is
+	//from the bottom or the left of the screen.
+	sf::Vector2f uniformMousePosition;
+
+	void updateKeyStates();
+	void updateMouseButtonStates();
+	void updateModifier(sf::Keyboard::Key modifierKey, bool isPressed);
 
 public:
-	InputState getKeyState(sf::Keyboard::Key inKey);
-	InputState getMouseButtonState(sf::Mouse::Button inButton);
-	sf::Vector2<float> getMousePosition();
+	KeyData& getKeyData(sf::Keyboard::Key inKey);
+	PushState getKeyState(sf::Keyboard::Key inKey);
+	PushState getMouseButtonState(sf::Mouse::Button inButton);
+	sf::Vector2f& getMousePosition();
+	sf::Vector2f& getUniformMousePosition();
 
 protected:
     void initialize() override;
@@ -36,8 +70,6 @@ protected:
 
     friend class GameEngine;
     DECLARE_SINGLETON(InputManager)
-
-
 };
 
 #endif
