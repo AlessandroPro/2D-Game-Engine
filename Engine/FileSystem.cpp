2ea5c2af14a4a99afbf9b57a149a30c0eac60347
Aditya Dinesh <adimondinesh@gmail.com>
2019-11-27 16:53:49 -0500

    Check for an existing conflict because the duplicate engine folder was deleted

--- Engine/FileSystem.cpp
+++ Engine/FileSystem.cpp
@@ -11,15 +11,20 @@ void FileSystem::initialize()
 
 void FileSystem::update(float deltaTime)
 {
-	if (removeFiles.size() > 0)					// if there is any file to unload
+	if (removeFiles.size() > 0)																// if there is any file to unload
 	{
 		for (auto iter : removeFiles)
 		{
+<<<<<<< HEAD
 			//GameObjectManager::instance().unloadFile(fileData.find(iter)->second);
 			//AssetManager::instance().unloadFile(fileData.find(iter)->second);
 			fileData.erase(fileData.find(iter));	// find the file in the cached data and removed it
+=======
+			fileData.erase(fileData.find(iter));											// find the file in the cached data and removed it
+>>>>>>> beb890ecde6c12b866d28ca03e781aaa782b997f
 		}
-		removeFiles.clear();					// clear the list used to store remove fileIds
+		removeFiles.clear();																// clear the list used to store remove fileIds
+		std::cout << "Remove all the files in the removeFile list" << std::endl;
 	}
 }
 
@@ -38,26 +43,29 @@ void FileSystem::load(std::string& fileName, bool protectedFile, bool isLevelFil
 		if (protectedFile == true && isLevelFile != true)				// check if the file is protected and not a level file
 		{
 			protectedFiles.push_back(fileId);
+			std::cout << "file is protected and not a level file" << std::endl;
 		}
 		else if (isLevelFile == true && protectedFile != true)			// check if the file is a level file and not protected. Then set it as current level
 		{
 			currentLevel = fileId;
+			std::cout << "file is a level file and catch the current level file." << std::endl;
+
 		}
 
-		//RenderSystem::instance().loadFIle(fileJSON);
-		//GameObjectManager::instance().loadFile(fileJSON);
-		//AssetManager::instance().loadFile(fileJSON);
+		//RenderSystem::instance().loadFile(fileJSON , fileId);
+		//GameObjectManager::instance().loadFile(fileJSON , fileId);
+		//AssetManager::instance().loadFile(fileJSON , fileId);
 	}
 }
 
 void FileSystem::unload(std::string& fileName)												//Method to unload a file
 {
-	STRCODE fileID = getHashCode(fileName.c_str());		// Convert filepath to STRCODE
-	bool isSafe = true;									// A check to see if file is safe to unload
+	STRCODE fileID = getHashCode(fileName.c_str());											// Convert filepath to STRCODE
+	bool isSafe = true;																		// A check to see if file is safe to unload
 
-	for (auto iter : protectedFiles)		// Iterate through 'protectedFiles' list to check if the file is protected
+	for (auto iter : protectedFiles)														// Iterate through 'protectedFiles' list to check if the file is protected
 	{
-		if (iter == fileId)		// If file is present in the list i.e protected file then dont unload
+		if (iter == fileId)																	// If file is present in the list i.e protected file then dont unload
 		{
 			isSafe = false;
 			MessageBox(NULL, TEXT("This file is protected and cannot be deleted"), TEXT("Alert"), MB_ABORTRETRYIGNORE | MB_ICONEXCLAMATION);
@@ -65,16 +73,26 @@ void FileSystem::unload(std::string& fileName)												//Method to unload a f
 		}
 	}
 
-	if (isSafe)		// if file is safe to unload then add to the 'removeFile' list which will be deleted on update
+	if (isSafe)						// if file is safe to unload then add to the 'removeFile' list which will be deleted on update
 	{
 		removeFiles.push_back(fileID);
+<<<<<<< HEAD
 		
+=======
+		//GameObjectManager::instance().unloadFile(fileJSON);
+		//AssetManager::instance().unloadFile(fileJSON);
+
+		std::cout << "store the files to be removed in the removeFiles List and remove it in the next update" << std::endl;
+
+>>>>>>> beb890ecde6c12b866d28ca03e781aaa782b997f
 	}
 }
 
 json::JSON FileSystem::loadAsset(std::string& fileName)										//pass the Json file to the Asset Manager
 {
-	
+	return parseJSON(fileName);
+	std::cout << "return json file to AssetManager" << std::endl;
+
 }
 
 json::JSON FileSystem::parseJSON(std::string& fileName)										//Method to parse the Json FIle 
@@ -82,6 +100,8 @@ json::JSON FileSystem::parseJSON(std::string& fileName)										//Method to par
 	std::ifstream inputStream(fileName);
 	std::string JSONstr((std::istreambuf_iterator<char>(inputStream)), std::istreambuf_iterator<char>());
 	return (json::JSON::Load(JSONstr));
+	std::cout << "Parse the JSON" << std::endl;
+
 }
 
 STRCODE FileSystem::getCurrentLevel()														//returns current loaded level
@@ -89,6 +109,7 @@ STRCODE FileSystem::getCurrentLevel()														//returns current loaded leve
 	if (currentLevel != NULL)
 	{
 		return currentLevel;
+		std::cout << "return the current level to the gameBojectManager" << std::endl;
 	}
 	else
 	{
