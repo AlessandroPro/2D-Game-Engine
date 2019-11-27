04e16b5d5859a76634de0aa746ddfa85f05a5ed5
Dakshvir Rehill <dakshvirr@gmail.com>
2019-11-27 15:02:45 -0500

    Edit : Added Remove and Create RigidBody functions into CollisionSystem

--- Engine/CollisionSystem.cpp
+++ Engine/CollisionSystem.cpp
@@ -34,11 +34,17 @@ void CollisionSystem::update(float deltaTime)
 	physicsWorld->Step(PHYSICS_TIME_STEP, PHYSICS_VELOCITY_ITERATIONS, PHYSICS_POSITION_ITERATIONS);
 }
 
+//Returns the created b2Body if physicsWorld is not null else returns null
 b2Body* CollisionSystem::CreateRigidBodyInWorld(b2BodyDef& bodyDefinition)
 {
-	return nullptr;
+	return physicsWorld == nullptr ? nullptr : physicsWorld->CreateBody(&bodyDefinition);
 }
 
+//Removes the b2Body from physicsWorld
 void CollisionSystem::RemoveRigidBodyFromWorld(b2Body* rigidBody)
 {
+	if (physicsWorld != nullptr)
+	{
+		physicsWorld->DestroyBody(rigidBody);
+	}
 }
