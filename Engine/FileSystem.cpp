3cf609c882ea7025aaecae85c2773661f734f7ab
shreyasbabu <shreyas.babu28@gmail.com>
2019-11-27 15:41:25 -0500

    ADD : output statments for merge request

--- Engine/FileSystem.cpp
+++ Engine/FileSystem.cpp
@@ -18,6 +18,7 @@ void FileSystem::update(float deltaTime)
 			fileData.erase(fileData.find(iter));											// find the file in the cached data and removed it
 		}
 		removeFiles.clear();																// clear the list used to store remove fileIds
+		std::cout << "Remove all the files in the removeFile list" << std::endl;
 	}
 }
 
@@ -36,15 +37,18 @@ void FileSystem::load(std::string& fileName, bool protectedFile, bool isLevelFil
 		if (protectedFile == true && isLevelFile != true)				// check if the file is protected and not a level file
 		{
 			protectedFiles.push_back(fileId);
+			std::cout << "file is protected and not a level file" << std::endl;
 		}
 		else if (isLevelFile == true && protectedFile != true)			// check if the file is a level file and not protected. Then set it as current level
 		{
 			currentLevel = fileId;
+			std::cout << "file is a level file and catch the current level file." << std::endl;
+
 		}
 
-		//RenderSystem::instance().loadFile(fileJSON);
-		//GameObjectManager::instance().loadFile(fileJSON);
-		//AssetManager::instance().loadFile(fileJSON);
+		//RenderSystem::instance().loadFile(fileJSON , fileId);
+		//GameObjectManager::instance().loadFile(fileJSON , fileId);
+		//AssetManager::instance().loadFile(fileJSON , fileId);
 	}
 }
 
@@ -68,12 +72,17 @@ void FileSystem::unload(std::string& fileName)												//Method to unload a f
 		removeFiles.push_back(fileID);
 		//GameObjectManager::instance().unloadFile(fileJSON);
 		//AssetManager::instance().unloadFile(fileJSON);
+
+		std::cout << "store the files to be removed in the removeFiles List and remove it in the next update" << std::endl;
+
 	}
 }
 
 json::JSON FileSystem::loadAsset(std::string& fileName)										//pass the Json file to the Asset Manager
 {
 	return parseJSON(fileName);
+	std::cout << "return json file to AssetManager" << std::endl;
+
 }
 
 json::JSON FileSystem::parseJSON(std::string& fileName)										//Method to parse the Json FIle 
@@ -81,6 +90,8 @@ json::JSON FileSystem::parseJSON(std::string& fileName)										//Method to par
 	std::ifstream inputStream(fileName);
 	std::string JSONstr((std::istreambuf_iterator<char>(inputStream)), std::istreambuf_iterator<char>());
 	return (json::JSON::Load(JSONstr));
+	std::cout << "Parse the JSON" << std::endl;
+
 }
 
 STRCODE FileSystem::getCurrentLevel()														//returns current loaded level
@@ -88,6 +99,7 @@ STRCODE FileSystem::getCurrentLevel()														//returns current loaded leve
 	if (currentLevel != NULL)
 	{
 		return currentLevel;
+		std::cout << "return the current level to the gameBojectManager" << std::endl;
 	}
 	else
 	{
