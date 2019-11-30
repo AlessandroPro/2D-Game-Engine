009e9bd151a36459e8426e53e14c2978656d61e6
Dakshvir Rehill <dakshvirr@gmail.com>
2019-11-30 23:22:11 +0000

    Revert "Edit: Added two new methods to set "strike through" and "underlined" styles...."
    This reverts commit 28da2617dd4b15e989b4b82e2c735c25a11e55bb

--- Engine/Text.h
+++ Engine/Text.h
@@ -20,23 +20,16 @@ class Text : public Component, IRenderable
 	STRCODE fontAssetID = -1;
 	sf::Font font;
 	std::string string;
-	//the three text modification settings below have been given default value examples in case the designer does not want to provide their own
-	//A possible characterSize assignment is:
-	//characterSize = 24;
-	int characterSize;
-	//A possible fillColor assignment is:
-	//fillColor = sf::Color::Black;
-	sf::Color fillColor;
+	//the three text modification settings below have been given default values in case the designer does not want to provide their own
+	int characterSize = 24;
+	sf::Color fillColor = sf::Color::Black;
+	sf::Text::Style style = sf::Text::Regular;
 	//All possible style assignments are shown below:
-	//style = sf::Text::Bold;
-	//style = sf::Text::Italic;
-	//style = sf::Text::Regular;
-	sf::Text::Style style;
-	//The other two styles below cannot be assigned to the style variable and must instead be added to the text through booleans:
-	//sf::Text::StrikeThrough;
-	//sf::Text::Underlined;
-	bool isTextStrikeThrough = false;
-	bool isTextUnderlined = false;
+	//sf::Text::Style style = sf::Text::Bold;
+	//sf::Text::Style style = sf::Text::Italic;
+	//sf::Text::Style style = sf::Text::Regular;
+	//sf::Text::Style style = sf::Text::StrikeThrough;
+	//sf::Text::Style style = sf::Text::Underlined;
 	Transform* transform = nullptr;
 
 public:
@@ -48,8 +41,6 @@ public:
 	void load(json::JSON& node);
 	void setText(sf::Font inFont, std::string inString, int inCharacterSize, sf::Color inFillColor, sf::Text::Style inStyle);
 	void setText(sf::Text* inText) { text = inText; }
-	void setIsStrikeThrough(bool inIsTextStrikeThrough) { isTextStrikeThrough = inIsTextStrikeThrough; }
-	void setIsUnderlined(bool inIsTextUnderlined) { isTextUnderlined = inIsTextUnderlined; }
 
 	friend class RenderSystem;
 };
