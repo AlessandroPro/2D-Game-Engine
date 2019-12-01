c5c0da5b9c293a738790aa3cf4c5d74ed44a6a30
Dakshvir Rehill <dakshvirr@gmail.com>
2019-11-30 19:28:16 -0500

    Edit : Removed transform reference and used getTransform function directly

--- Engine/Camera.cpp
+++ Engine/Camera.cpp
@@ -1,32 +1,26 @@
 #include "Core.h"
-#include "Camera.h"
 #include "RenderSystem.h"
 #include "GameObject.h"
 #include "Transform.h"
-#include "Component.h"
+#include "Camera.h"
 
 IMPLEMENT_DYNAMIC_CLASS(Camera)
 
 void Camera::initialize()
 {
 	Component::initialize();
-
-	if (transform == nullptr)
-	{
-		transform = getGameObject()->getTransform();
-	}
 }
 
 void Camera::update(float deltaTime)
 {
-	if(transform != nullptr)
+	if(getGameObject()->getTransform() != nullptr)
 	{
 		//Update the position of the view on the window to be equal to the position of the transform of the camera object
-		RenderSystem::instance().currentView.setCenter(transform->getPosition());
+		RenderSystem::instance().currentView.setCenter(getGameObject()->getTransform()->getPosition());
 	}
 }
 
-void Camera::load(json::JSON loadNode)
+void Camera::load(json::JSON& loadNode)
 {
 	Component::load(loadNode);
 }
--- Engine/Camera.h
+++ Engine/Camera.h
@@ -1,17 +1,12 @@
 #pragma once
 #include "Component.h"
-#include "Transform.h"
-
 class Camera : public Component
 {
 	DECLARE_DYNAMIC_DERIVED_CLASS(Camera, Component)
 
-private:
-	Transform* transform;
-
 public:
 	void initialize() override;
 	void update(float deltaTime) override;
-	void load(json::JSON loadNode);
+	void load(json::JSON& loadNode) override;
 };
 
